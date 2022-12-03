c25e976f3aee942882380db9615872cb928db57a
Eric Clemente <eeric20011@gmail.com>
2022-12-03 22:37:16 +0100

    Update: day1 solution

--- day1/wrapping.js
+++ day1/wrapping.js
@@ -1,6 +1,8 @@
-export function wrapping(gifts) {
-  return gifts.map((g) => {
-    const a = "*".repeat(g.length + 2);
-    return `${a}\n*${g}*\n${a}`;
-  });
-}
+export const wrapping = (g) => g.map((g) => `${"*".repeat(g.length + 2)}\n*${g}*\n${"*".repeat(g.length + 2)}`);
+
+// export function wrapping(gifts) {
+//   return gifts.map((gift) => {
+//     const asterisks = "*".repeat(gift.length + 2);
+//     return `${asterisks}\n*${gift}*\n${asterisks}`;
+//   });
+// }
