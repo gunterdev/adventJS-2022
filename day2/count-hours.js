f67242fced626ca544443ef6da0dcff6a00bb7e5
Eric Clemente <eeric20011@gmail.com>
2022-12-03 22:37:28 +0100

    Add: day2 solution

---
+++ day2/count-hours.js
@@ -0,0 +1,8 @@
+const countHours = (y, h) => h.reduce((e, h) => e + ((w = new Date(`${y}-${h}`).getDay()) < 6 && w > 0 ? 2 : 0), 0);
+
+// function countHours(year, holidays) {
+//   return holidays.reduce(
+//     (extra, holiday) => extra + ((workday = new Date(`${year}-${holiday}`).getDay()) < 6 && workday > 0 ? 2 : 0),
+//     0
+//   );
+// }
